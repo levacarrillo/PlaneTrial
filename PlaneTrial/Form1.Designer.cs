928e9da99cf5712c43d36c2163750d1c184b6027
Luis Nava <levacarrillo@gmail.com>
2021-04-05 21:23:59 -0500

    Added soundtrack game.

--- PlaneTrial/Form1.Designer.cs
+++ PlaneTrial/Form1.Designer.cs
@@ -29,13 +29,114 @@ namespace PlaneTrial
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.start = new System.Windows.Forms.ToolStripMenuItem();
+            this.difficult = new System.Windows.Forms.ToolStripMenuItem();
+            this.easy = new System.Windows.Forms.ToolStripMenuItem();
+            this.middle = new System.Windows.Forms.ToolStripMenuItem();
+            this.hard = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructions = new System.Windows.Forms.ToolStripMenuItem();
+            this.pause = new System.Windows.Forms.ToolStripMenuItem();
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.start,
+            this.difficult,
+            this.instructions,
+            this.pause,
+            this.exit});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // start
+            // 
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(54, 24);
+            this.start.Text = "Start";
+            this.start.Click += new System.EventHandler(this.start_ButtonClick);
+            // 
+            // difficult
+            // 
+            this.difficult.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easy,
+            this.middle,
+            this.hard});
+            this.difficult.Name = "difficult";
+            this.difficult.Size = new System.Drawing.Size(76, 24);
+            this.difficult.Text = "Difficult";
+            // 
+            // easy
+            // 
+            this.easy.Name = "easy";
+            this.easy.Size = new System.Drawing.Size(139, 26);
+            this.easy.Text = "Easy";
+            // 
+            // middle
+            // 
+            this.middle.Name = "middle";
+            this.middle.Size = new System.Drawing.Size(139, 26);
+            this.middle.Text = "Middle";
+            // 
+            // hard
+            // 
+            this.hard.Name = "hard";
+            this.hard.Size = new System.Drawing.Size(139, 26);
+            this.hard.Text = "Hard";
+            // 
+            // instructions
+            // 
+            this.instructions.Name = "instructions";
+            this.instructions.Size = new System.Drawing.Size(98, 24);
+            this.instructions.Text = "Instructions";
+            // 
+            // pause
+            // 
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(60, 24);
+            this.pause.Text = "Pause";
+            this.pause.Click += new System.EventHandler(this.pause_ButtonClick);
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(47, 24);
+            this.exit.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem start;
+        private System.Windows.Forms.ToolStripMenuItem difficult;
+        private System.Windows.Forms.ToolStripMenuItem instructions;
+        private System.Windows.Forms.ToolStripMenuItem pause;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+        private System.Windows.Forms.ToolStripMenuItem easy;
+        private System.Windows.Forms.ToolStripMenuItem middle;
+        private System.Windows.Forms.ToolStripMenuItem hard;
     }
 }
 
--- PlaneTrial/Form1.cs
+++ PlaneTrial/Form1.cs
@@ -7,15 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
 
 namespace PlaneTrial
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
+        private string soundtrack_path = Path.Combine( Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName,
+                                                       "Fatboy_Slim_-_Acid_8000.mp3");
+        public Form1() {
+
             InitializeComponent();
+
+            Mp3Player.open_file(soundtrack_path);
         }
 
+        private void start_ButtonClick(object sender, EventArgs e) {
+
+            Mp3Player.play();
+        }
+
+        private void pause_ButtonClick(object sender, EventArgs e) {
+
+            Mp3Player.stop();
+        }
+    }
+
+    public class Mp3Player
+    {
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string lpstrCommand, StringBuilder lpstrReturnString,
+                                                   int uReturnLength, int hwdCallBack);
+        public static void open_file(string File)
+        {
+
+            string Format = @"open ""{0}"" type MPEGVideo alias MediaFile";
+            string command = string.Format(Format, File);
+            mciSendString(command, null, 0, 0);
+            Debug.WriteLine("Sound track path->" + File);
+        }
+
+        public static void play()
+        {
+
+            string command = "Play MediaFile";
+            mciSendString(command, null, 0, 0);
+            Debug.WriteLine("Playing soundtrack");
+        }
+        public static void stop()
+        {
+
+            string command = "stop MediaFile";
+            mciSendString(command, null, 0, 0);
+            Debug.WriteLine("Stopping soundtrack");
+        }
     }
 }
